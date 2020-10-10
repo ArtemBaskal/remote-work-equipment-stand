421d9c496ec17fc0ed64c869dee183df0219ee18
ArtemBaskal <a.baskal@adguard.com>
2020-10-10 17:54:31 +0300

    Update styles, add file uploader

--- client/src/app/App.tsx
+++ client/src/app/App.tsx
@@ -1,6 +1,18 @@
 import React from 'react';
 import Auth from 'features/auth/Auth';
+import FileUploader from 'components/FileUploader';
+import { useSelector } from 'react-redux';
 
-const App = () => <Auth />;
+const App = () => {
+  const name = useSelector((state: any) => state.auth.name);
+  const isAuthenticated = !!name;
+
+  return (
+    <>
+      <Auth />
+      {isAuthenticated && <FileUploader />}
+    </>
+  );
+};
 
 export default App;
--- client/src/features/auth/Auth.tsx
+++ client/src/features/auth/Auth.tsx
@@ -2,16 +2,32 @@ import React from 'react';
 import { GoogleLogin, GoogleLogout } from 'react-google-login';
 import { getProfile, clearProfile } from 'features/auth/authSlice';
 import { shallowEqual, useDispatch, useSelector } from 'react-redux';
-import { Avatar } from '@material-ui/core';
+import {
+  Avatar,
+  List,
+  ListItem,
+  ListItemAvatar,
+  ListItemSecondaryAction,
+  ListItemText,
+  Toolbar,
+} from '@material-ui/core';
 import API_KEYS from 'API_KEYS.json';
+import { makeStyles } from '@material-ui/core/styles';
 
-function Auth() {
+const useStyles = makeStyles(() => ({
+  root: {
+    flexGrow: 1,
+  },
+}));
+
+const Auth = () => {
   const dispatch = useDispatch();
   const {
     imageUrl,
     email,
     name,
   } = useSelector((state: any) => state.auth, shallowEqual);
+  const classes = useStyles();
 
   const isAuthenticated = !!name;
 
@@ -28,28 +44,44 @@ function Auth() {
   };
 
   return (
-    <>
-      {isAuthenticated && <Avatar alt={name} src={imageUrl} />}
-      <div>{email}</div>
-      <div>{name}</div>
-      {isAuthenticated ? (
-        <GoogleLogout
-          clientId={API_KEYS.GOOGLE_AUTH}
-          buttonText="Выйти"
-          onLogoutSuccess={onLogoutSuccess}
-        />
-      ) : (
-        <GoogleLogin
-          clientId={API_KEYS.GOOGLE_AUTH}
-          buttonText="Войти"
-          onSuccess={onSuccess}
-          onFailure={onFailure}
-          cookiePolicy="single_host_origin"
-          isSignedIn
-        />
-      )}
-    </>
+    <Toolbar>
+      <div className={classes.root}>
+        <List>
+          <ListItem>
+            {name && (
+            <ListItemAvatar>
+              <Avatar>
+                <Avatar alt={name} src={imageUrl} />
+              </Avatar>
+            </ListItemAvatar>
+            )}
+            <ListItemText
+              primary={name}
+              secondary={email}
+            />
+            <ListItemSecondaryAction>
+              {isAuthenticated ? (
+                <GoogleLogout
+                  clientId={API_KEYS.GOOGLE_AUTH}
+                  buttonText="Выйти"
+                  onLogoutSuccess={onLogoutSuccess}
+                />
+              ) : (
+                <GoogleLogin
+                  clientId={API_KEYS.GOOGLE_AUTH}
+                  buttonText="Войти"
+                  onSuccess={onSuccess}
+                  onFailure={onFailure}
+                  cookiePolicy="single_host_origin"
+                  isSignedIn
+                />
+              )}
+            </ListItemSecondaryAction>
+          </ListItem>
+        </List>
+      </div>
+    </Toolbar>
   );
-}
+};
 
 export default Auth;
